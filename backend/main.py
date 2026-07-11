7b49e019605da4f3419ef86da8209453cf5eac1a
Ricardo <rickyamal@gmail.com>
2026-07-10 22:12:32 -0300

    feat: tem certeza deploy na sexta?

--- backend/main.py
+++ backend/main.py
@@ -27,6 +27,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+@app.get("/health")
+def health():
+    return {
+        "status": "ok"
+    }
+
 # Inclui as rotas do arquivo routers/auth.py
 app.include_router(auth.router)
 app.include_router(practice.router)
